600b6fe7baa0f6b7e0d6451c1d8e8ddbcf154038
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-01-18 23:05:52 +0400

    Fix for modal - When we show modal, we must hide other modals; Add new future for image-blend. Now we can set gradient color in options;

--- resources/views/partials/hero.blade.php
+++ resources/views/partials/hero.blade.php
@@ -1,4 +1,4 @@
-<section class="hero hero--overlay-layer position-relative padding-y-xxl" data-blend-pattern="0,0,1,0" style="background-image: url('/assets/img/intro-img-1.jpg')">
+<section class="hero hero--overlay-layer position-relative padding-y-xxl img-blend" data-blend-pattern="0,0,1,0" data-blend-color="--color-bg" style="background-image: url('/assets/img/intro-img-1.jpg')">
   <div class="container max-width-adaptive-sm">
     <div class="position-relative z-index-2 text-center">
       <div class="text-sm opacity-60% margin-bottom-xxs">This is a Label</div>
@@ -7,7 +7,7 @@
         <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam vero aliquam, natus quae architecto hic quod!</p>
       </div>
-  
+
       <div class="flex flex-wrap flex-center gap-sm">
         <a href="#0" class="btn btn--primary">Download</a>
         <a href="#0" class="color-inherit">Learn more</a>
