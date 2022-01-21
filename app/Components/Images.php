136f39f389979432712184905eb12bf4779719c4
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-01-21 23:25:55 +0400

    Replace random background image folder;

--- app/Components/Images.php
+++ app/Components/Images.php
@@ -2,14 +2,17 @@
 
 namespace App\Components;
 
+use Illuminate\Support\Facades\File;
+
 trait Images
 {
     public static function randomBackground(){
+        $folder = '/assets/img/random-backgrounds/';
         // Getting all images from target folder;
-        $files = \Illuminate\Support\Facades\Storage::files('public/random-backgrounds');
+        $files = File::files(public_path().$folder);
         // Get random image from file list;
-        $rand_image = $files[rand(0, count($files)-1)];
+        $rand_image = $files[rand(0, count($files)-1)]->getFilename();
         // Replace some useless chars and return result;
-        return str_replace('public', '/storage', $rand_image);
+        return $folder.$rand_image;
     }
 }
