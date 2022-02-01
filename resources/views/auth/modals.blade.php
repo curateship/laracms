95a86cd0a3a382317b04ed0f5d9c700d83c9e62d
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-02-01 13:44:49 +0400

    Fix for showing login and signup modals; Fix for errors showing fin login and signup modals;

--- resources/views/auth/modals.blade.php
+++ resources/views/auth/modals.blade.php
@@ -3,27 +3,33 @@
   <div class="modal__content width-100% max-width-xs max-height-100% overflow-auto padding-md bg radius-md inner-glow shadow-md" role="alertdialog" aria-labelledby="modal-form-title" aria-describedby="modal-form-description">
 
   <!-- Login Form Start 👇-->
-  <form action="{{ route('login') }}" method="POST">@csrf
-    <form class="login-form">
+  <form action="{{ route('login') }}" class="modal-login" method="POST">
+      @csrf
+      <input type="hidden" name="target" value="modal-login">
+
       <div class="text-component text-center margin-bottom-sm">
         <h1>Log in</h1>
       </div>
 
       <div class="margin-bottom-sm">
-        @error('email')
-          <span class="form-control--error" role="alert">
-            {{ $message }}
-          </span>
-        @enderror
+          @if(old('target') == 'modal-login')
+              @error('email')
+              <span class="form-control--error" role="alert">
+                {{ $message }}
+              </span>
+              @enderror
+          @endif
         <input class="form-control width-100%" type="email" name="email" id="input-email" placeholder="email@myemail.com">
       </div>
 
       <div class="margin-bottom-sm">
-        @error('password')
-        <span class="form-control--error" role="alert">
-          {{ $message }}
-        </span>
-        @enderror
+          @if(old('target') == 'modal-login')
+              @error('password')
+              <span class="form-control--error" role="alert">
+                  {{ $message }}
+                </span>
+              @enderror
+          @endif
         <input class="form-control width-100%" type="password" name="password" id="input-password" placeholder="Input Password">
         <span class="text-sm"><a href="{{ route('password.request') }}" aria-controls="modal-forgot-password">Forgot Your Password?</a></span>
       </div>
@@ -35,7 +41,7 @@
       <div class="text-center">
         <p class="text-sm">Don't have an account? <a href="{{ route('register') }}" aria-controls="modal-signup">Get started</a></p>
       </div>
-  </form>
+
 </form>
 
 <button class="reset modal__close-btn modal__close-btn--outer  js-modal__close js-tab-focus">
@@ -59,8 +65,9 @@
     <div class="max-width-xs margin-x-auto">
 
     <!-- Register Form Content 👇-->
-<form action="{{ route('register') }}" method="POST">@csrf
-  <form class="sign-up-form padding-lg">
+<form action="{{ route('register') }}" class="sign-up-form padding-lg" method="POST">
+    @csrf
+    <input type="hidden" name="target" value="modal-signup">
 
     <div class="text-component text-center margin-bottom-sm">
       <h1>Get started</h1>
@@ -68,38 +75,46 @@
     </div>
 
     <div class="padding-bottom-sm">
-      @error('name')
-      <span class="form-control--error" role="alert">
-      <strong>{{ $message }}</strong>
-      </span>
-      @enderror
+        @if(old('target') == 'modal-signup')
+            @error('name')
+            <span class="form-control--error" role="alert">
+              <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+        @endif
       <input class="form-control width-100%" type="text" name="name" id="name" placeholder="Enter Name">
     </div>
 
     <div class="margin-bottom-sm">
-      @error('email')
-      <span class="form-control--error" role="alert">
-      <strong>{{ $message }}</strong>
-      </span>
-      @enderror
+        @if(old('target') == 'modal-signup')
+            @error('email')
+            <span class="form-control--error" role="alert">
+              <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+        @endif
       <input class="form-control width-100%" type="email" name="email" id="input-email" placeholder="email@myemail.com">
     </div>
 
     <div class="margin-bottom-sm">
-      @error('password')
-      <span class="form-control--error" role="alert">
-      <strong>{{ $message }}</strong>
-      </span>
-      @enderror
+        @if(old('target') == 'modal-signup')
+            @error('password')
+            <span class="form-control--error" role="alert">
+              <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+        @endif
       <input class="form-control width-100%" type="password" name="password" id="input-password" placeholder="Input Password">
     </div>
 
     <div class="margin-bottom-md">
-      @error('password')
-      <span class="form-control--error" role="alert">
-      <strong>{{ $message }}</strong>
-      </span>
-      @enderror
+        @if(old('target') == 'modal-signup')
+            @error('password')
+            <span class="form-control--error" role="alert">
+              <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+        @endif
       <input class="form-control width-100%" type="password" name="password_confirmation" id="input-password" placeholder="Enter Password Again">
       <p class="text-xs color-contrast-medium margin-top-xxs">Minimum 8 characters</p>
     </div>
@@ -111,8 +126,6 @@
     <div class="text-center">
       <p class="text-xs color-contrast-medium">By joining, you agree to our <a href="#0">Terms</a> and <a href="#0">Privacy Policy</a>.</p>
     </div>
-
-  </form>
 </form>
 <!-- Register Form Content END -->
 
@@ -136,8 +149,7 @@
   <div class="modal__content width-100% max-width-xs max-height-100% overflow-auto padding-md bg radius-md inner-glow shadow-md" role="alertdialog" aria-labelledby="modal-form-title" aria-describedby="modal-form-description">
 
   <!-- Forgot Password Form Start 👇-->
-<form action="{{ route('password.request') }}" method="POST">@csrf
-<form class="password-reset-form">
+<form action="{{ route('password.request') }}" class="password-reset-form" method="POST">@csrf
   <div class="text-component text-center margin-bottom-md">
     <h1>Forgot your password?</h1>
     <p>Enter your Email below</p>
@@ -164,7 +176,6 @@
   <div class="text-center">
     <p class="text-sm"><a href="{{ route('login') }}" aria-controls="modal-login">&larr; Back to login</a></p>
   </div>
-</form>
 </form>
 <!-- Forgot Password Form END-->
 
