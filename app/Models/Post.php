fca41b6bf79dafed300972e8b94238f8a615642f
Tyler <mobileglobalweb@gmail.com>
2022-02-02 15:27:20 -0500

    Created Category Model and Migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Category;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug', 'excerpt', 'body', 'user_id', 'category_id'];
+
     public function author()
-{
-	return $this->belongsTo(User::class, 'user_id');
-}
+    {
+	    return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category()
+    {
+	    return $this->belongsTo(User::class);
+    }
 	
 }
