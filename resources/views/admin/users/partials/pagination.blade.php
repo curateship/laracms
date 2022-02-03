ca333cb13c79836c8cc085279ef1e70cbe15e981
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-02-03 16:49:47 +0400

    User pagination buttons limit;

--- resources/views/admin/users/partials/pagination.blade.php
+++ resources/views/admin/users/partials/pagination.blade.php
@@ -7,11 +7,33 @@
       </a>
     </li>
 
-      @for ($i = 1; $i <= $users->lastPage(); $i++)
+      @if($users->currentPage() > 3)
           <li class="display@sm">
-              <a href="{{ $users->url($i) }}" class="pagination__item {{ ($users->currentPage() == $i) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $i }}">{{ $i }}</a>
+              <a href="{{ $users->url(1) }}" class="pagination__item {{ ($users->currentPage() == 1) ? ' pagination__item--selected' : '' }}" aria-label="Go to page 1">1</a>
           </li>
-      @endfor
+      @endif
+      @if($users->currentPage() > 4)
+          <li class="display@sm" aria-hidden="true">
+              <span class="pagination__item pagination__item--ellipsis">...</span>
+          </li>
+      @endif
+      @foreach(range(1, $users->lastPage()) as $i)
+          @if($i >= $users->currentPage() - 2 && $i <= $users->currentPage() + 2)
+              <li class="display@sm">
+                  <a href="{{ $users->url($i) }}" class="pagination__item {{ ($users->currentPage() == $i) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $i }}">{{ $i }}</a>
+              </li>
+          @endif
+      @endforeach
+      @if($users->currentPage() < $users->lastPage() - 3)
+          <li class="display@sm" aria-hidden="true">
+              <span class="pagination__item pagination__item--ellipsis">...</span>
+          </li>
+      @endif
+      @if($users->currentPage() < $users->lastPage() - 2)
+          <li class="display@sm">
+              <a href="{{ $users->url($users->lastPage()) }}" class="pagination__item {{ ($users->currentPage() == $users->lastPage()) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $users->lastPage() }}">{{ $users->lastPage() }}</a>
+          </li>
+      @endif
 
     <li>
       <a href="{{ $users->url($users->currentPage()+1) }}" class="pagination__item {{ ($users->currentPage() == $users->lastPage()) ? ' pagination__item--disabled' : '' }}" aria-label="Go to next page">
