6ca7349b6684ff6a81934299a0a60233dd7c78bc
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-02-19 22:26:07 +0400

    Fix modal close; Fix close button;

--- resources/views/theme/default/auth/modals.blade.php
+++ resources/views/theme/default/auth/modals.blade.php
@@ -129,19 +129,18 @@
     </div>
 </form>
 <!-- Register Form Content END -->
+        <button class="reset modal__close-btn modal__close-btn--outer  js-modal__close js-tab-focus">
+            <svg class="icon icon--sm" viewBox="0 0 24 24">
+                <title>Close modal window</title>
+                <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="3" y1="3" x2="21" y2="21" />
+                    <line x1="21" y1="3" x2="3" y2="21" />
+                </g>
+            </svg>
+        </button>
 
     </div>
   </div>
-
-  <button class="reset modal__close-btn modal__close-btn--outer  js-modal__close js-tab-focus">
-    <svg class="icon icon--sm" viewBox="0 0 24 24">
-      <title>Close modal window</title>
-      <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <line x1="3" y1="3" x2="21" y2="21" />
-        <line x1="21" y1="3" x2="3" y2="21" />
-      </g>
-    </svg>
-  </button>
 </div>
 <!-- Signup Modal END-->
 
@@ -181,6 +180,15 @@
     <p class="text-sm"><a href="{{ route('login') }}" aria-controls="modal-login">&larr; Back to login</a></p>
   </div>
 </form>
+      <button class="reset modal__close-btn modal__close-btn--outer  js-modal__close js-tab-focus">
+          <svg class="icon icon--sm" viewBox="0 0 24 24">
+              <title>Close modal window</title>
+              <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <line x1="3" y1="3" x2="21" y2="21" />
+                  <line x1="21" y1="3" x2="3" y2="21" />
+              </g>
+          </svg>
+      </button>
 <!-- Forgot Password Form END-->
 
 </div>
