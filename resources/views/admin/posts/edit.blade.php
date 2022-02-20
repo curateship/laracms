21bdd9e4eeb59040f5815a39c2ec099182a7ed2d
Tyler <mobileglobalweb@gmail.com>
2022-02-20 18:53:48 -0500

    Fix post edit page UI

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -40,28 +40,12 @@
     <div class="flex flex-wrap items-center justify-between margin-right-sm">
       <div class="flex flex-wrap">
         <menu class="menu-bar js-int-table-actions__no-items-selected js-menu-bar">
-          <li class="menu-bar__item" role="menuitem" aria-controls="create-modal">
-            <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
-               <path d="M18.85 4.39l-3.32-3.32a0.83 0.83 0 0 0-1.18 0l-11.62 11.62a0.84 0.84 0 0 0-0.2 0.33l-1.66 4.98a0.83 0.83 0 0 0 0.79 1.09 0.84 0.84 0 0 0 0.26-0.04l4.98-1.66a0.84 0.84 0 0 0 0.33-0.2l11.62-11.62a0.83 0.83 0 0 0 0-1.18z m-6.54 1.08l1.17-1.18 2.15 2.15-1.18 1.17z"></path>
-            </svg>
-            <span class="menu-bar__label">Add Post</span>
-          </li>
 
           <li class="menu-bar__item" role="menuitem" aria-controls="post-search">
             <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
               <path d="M11.25 17.5c4.83 0 8.75-3.93 8.75-8.75s-3.93-8.75-8.75-8.75-8.75 3.93-8.75 8.75 3.93 8.75 8.75 8.75z m0-15c3.45 0 6.25 2.8 6.25 6.25s-2.8 6.25-6.25 6.25-6.25-2.8-6.25-6.25 2.8-6.25 6.25-6.25z"></path><path d="M0.36 17.86l3-2.99a10.02 10.02 0 0 0 1.76 1.77l-2.98 3a1.25 1.25 0 0 1-1.78 0 1.25 1.25 0 0 1 0-1.78z"></path>
             </svg>
             <span class="menu-bar__label">Search Posts</span>
-          </li>
-        </menu>
-
-        <menu class="menu-bar is-hidden js-int-table-actions__items-selected js-menu-bar">
-          <li class="menu-bar__item" role="menuitem">
-            <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 16 16">
-              <g><path d="M2,6v8c0,1.1,0.9,2,2,2h8c1.1,0,2-0.9,2-2V6H2z"></path><path d="M12,3V1c0-0.6-0.4-1-1-1H5C4.4,0,4,0.4,4,1v2H0v2h16V3H12z M10,3H6V2h4V3z"></path></g>
-            </svg>
-            <span class="counter counter--critical counter--docked">4 <i class="sr-only">Notifications</i></span>
-            <span class="menu-bar__label">Delete</span>
           </li>
         </menu>
 
@@ -142,7 +126,7 @@
     <!-- Select Category Dropdown Autocomplete END -->
 
     <div class="margin-bottom-sm">
-      <input class="form-control width-100%" type="name" name="inputname" id="inputname" placeholder="Enter Post Slug">
+      <input class="form-control width-100%" type="name" name="inputname" id="inputname" placeholder="Edit Post Slug">
     </div>
 
     <!-- Image Upload -->
@@ -151,7 +135,7 @@
       <span class="flex items-center">
         <svg class="icon" viewBox="0 0 24 24" aria-hidden="true"><g fill="none" stroke="currentColor" stroke-width="2"><path  stroke-linecap="square" stroke-linejoin="miter" d="M2 16v6h20v-6"></path><path stroke-linejoin="miter" stroke-linecap="butt" d="M12 17V2"></path><path stroke-linecap="square" stroke-linejoin="miter" d="M18 8l-6-6-6 6"></path></g></svg>
         
-        <span class="margin-left-xxs file-upload__text file-upload__text--has-max-width">Upload Image</span>
+        <span class="margin-left-xxs file-upload__text file-upload__text--has-max-width">Edit Image</span>
       </span>
     </label> 
   
@@ -162,9 +146,7 @@
   </fieldset>
 
   <div class="flex justify-end gap-xs">
-        <button class="btn btn--subtle js-modal__close">Cancel</button>
-        <button class="btn btn--accent">Save</button>
-        <button class="btn btn--primary">Publish</button>
+        <button class="btn btn--primary">Save</button>
       </div>
    </form>
 </div>
