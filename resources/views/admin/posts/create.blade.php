634ed91a7aa7b98c8246bcbb05431fcc85178b47
Tyler <mobileglobalweb@gmail.com>
2022-02-20 15:51:44 -0500

    Change paragraph padding. Cleaned up UI

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -45,12 +45,6 @@
     <div class="flex flex-wrap items-center justify-between margin-right-sm">
       <div class="flex flex-wrap">
         <menu class="menu-bar js-int-table-actions__no-items-selected js-menu-bar">
-          <li class="menu-bar__item" role="menuitem" aria-controls="create-modal">
-            <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
-               <path d="M18.85 4.39l-3.32-3.32a0.83 0.83 0 0 0-1.18 0l-11.62 11.62a0.84 0.84 0 0 0-0.2 0.33l-1.66 4.98a0.83 0.83 0 0 0 0.79 1.09 0.84 0.84 0 0 0 0.26-0.04l4.98-1.66a0.84 0.84 0 0 0 0.33-0.2l11.62-11.62a0.83 0.83 0 0 0 0-1.18z m-6.54 1.08l1.17-1.18 2.15 2.15-1.18 1.17z"></path>
-            </svg>
-            <span class="menu-bar__label">Add Post</span>
-          </li>
 
           <li class="menu-bar__item" role="menuitem" aria-controls="post-search">
             <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
@@ -60,23 +54,13 @@
           </li>
         </menu>
 
-        <menu class="menu-bar is-hidden js-int-table-actions__items-selected js-menu-bar">
-          <li class="menu-bar__item" role="menuitem">
-            <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 16 16">
-              <g><path d="M2,6v8c0,1.1,0.9,2,2,2h8c1.1,0,2-0.9,2-2V6H2z"></path><path d="M12,3V1c0-0.6-0.4-1-1-1H5C4.4,0,4,0.4,4,1v2H0v2h16V3H12z M10,3H6V2h4V3z"></path></g>
-            </svg>
-            <span class="counter counter--critical counter--docked">4 <i class="sr-only">Notifications</i></span>
-            <span class="menu-bar__label">Delete</span>
-          </li>
-        </menu>
-
       </div><!-- END Control Bar -->
     </div>
   </div><!-- END card -->
 <!-- END Control Bar-->
 
 <!-- Table-->
-<div class="margin-top-auto border-top border-contrast-lower"></div><!-- Divider -->
+<div class="margin-top-auto border-top border-contrast-lower opacity-40%"></div><!-- Divider -->
 <div class="padding-md">
 <form method="POST" action="{{ route('post.store') }}" id="new-post-form">
     @csrf
