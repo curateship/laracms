d7d9c6e3caca85ed308a168f4248e1595b9132c8
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-02-21 20:45:52 +0400

    Input fix;

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,4 +1,4 @@
-@props(['type' => 'text', 'name', 'placeholder', 'required' => 'true', 'value'])
+@props(['type' => 'text', 'name', 'placeholder', 'required' => 'true', 'value' => ''])
 
 <input value='{{ $value }}' {{ $required == 'true' ? 'required' : '' }} type="{{ $type }}" id="{{ $name }}" name='{{ $name }}' class="form-control width-100%" placeholder="{{ $placeholder }}">
 
