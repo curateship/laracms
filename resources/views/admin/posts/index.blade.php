ee0fdcdd50b110ff87b662a68bb9a05971a816c0
Tyler <mobileglobalweb@gmail.com>
2022-02-22 20:35:50 -0500

    Add preview link to admin post

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -238,7 +238,7 @@
                               <path d="M15,4H1C0.4,4,0,4.4,0,5v10c0,0.6,0.4,1,1,1h14c0.6,0,1-0.4,1-1V5C16,4.4,15.6,4,15,4z M14,14H2V6h12V14z"></path>
                               <rect x="2" width="12" height="2"></rect>
                             </svg>
-                            <span>Preview</span>
+                            <span><a class="text-decoration-none color-inherit" href="{{ route('post.show', $post) }}" target="_blank">Preview</a></span>
                           </span>
                               </li>
 
