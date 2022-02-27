8393600559cef37786305dc3c143a1b0b2ae9bd6
Tyler <mobileglobalweb@gmail.com>
2022-02-27 12:41:40 -0500

    Change create and edit label for post

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -141,9 +141,8 @@
       @foreach(\App\Models\TagsCategories::all() as $key=> $tag_category)
           <div class="grid margin-bottom-sm">
               <label class="form-label margin-bottom-xxxs" for="tag_category_{{ $tag_category->id }}">
-                  Add {{ $tag_category->name }}
               </label>
-              <select name="tag_category_{{ $tag_category->id }}[]" id="tag_category_{{ $tag_category->id }}" class="site-tag-pills" data-id="{{ $tag_category->id }}" data-placeholder="Select a Tags for {{ $tag_category->name }}" multiple>
+              <select name="tag_category_{{ $tag_category->id }}[]" id="tag_category_{{ $tag_category->id }}" class="site-tag-pills" data-id="{{ $tag_category->id }}" data-placeholder="Edit {{ $tag_category->name }}" multiple>
                   @foreach($post->tags($tag_category->id) as $tag)
                       <option value="{{$tag->id}}" selected>{{$tag->name}}</option>
                   @endforeach()
