40ca790d0f01f1887f6ca147b2e131819709ba33
Tyler <mobileglobalweb@gmail.com>
2022-03-07 19:10:34 -0500

    Add SEO title to index

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Frontend;
 // Others
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Artesaos\SEOTools\Facades\SEOTools;
+use Artesaos\SEOTools\Facades\SEOMeta;
 
 // Models
 use App\Models\Post;
@@ -18,6 +20,7 @@ class IndexController extends Controller
 {
     public function index()
     {
+        SEOMeta::setTitle('Site Title');
         $recent_posts = Post::latest()->withCount('comments')->with('author')->paginate(10);
         return view('/themes.jpn.index', [
         'recent_posts' => $recent_posts,
