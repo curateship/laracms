40cdd45ff08cedf9023d365a53d066091b78bdd1
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-03-09 18:43:19 +0400

    Animation uploading fix;

--- app/Http/Controllers/Admin/AdminPostController.php
+++ app/Http/Controllers/Admin/AdminPostController.php
@@ -24,6 +24,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Imagick;
 
 class AdminPostController extends Controller
 {
