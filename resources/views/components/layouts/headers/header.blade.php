9707f4d3dd44c57377ea300cb5ddd798db6caed2
Tyler <mobileglobalweb@gmail.com>
2022-03-09 19:38:59 -0500

    Added component svg to git ignore

--- resources/views/components/layouts/headers/header.blade.php
+++ resources/views/components/layouts/headers/header.blade.php
@@ -18,7 +18,7 @@
           @auth
           <button class="header-v2__nav-control reset anim-menu-btn js-anim-menu-btn switch-icon switch-icon--rotate js-switch-icon js-tab-focus" aria-label="Toggle icon" menu-target="user-menu">
               <div class="mega-nav__icon-btn dropdown__wrapper inline-block author author--minimal-mobile switch-icon__icon switch-icon__icon--a">
-                  <div class="author__img-wrapper author--minimal-mobile dropdown__trigger object-cover">
+                  <div class="author__img-wrapper author--minimal-mobile dropdown__trigger object-cover margin-top-xxxs">
                       <img src="{{ url('/storage'.config('images.users_storage_path').\Illuminate\Support\Facades\Auth::user()->thumbnail)  }}" alt="Logged in user avatar">
                   </div>
               </div>
