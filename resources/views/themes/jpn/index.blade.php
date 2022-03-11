455a56f43e1b338aea49e1a1c4cc98ce67413fb5
Tyler <mobileglobalweb@gmail.com>
2022-03-10 20:56:39 -0500

    Changed login route to index

--- resources/views/themes/jpn/index.blade.php
+++ resources/views/themes/jpn/index.blade.php
@@ -9,19 +9,17 @@
         @include('components.posts.lists.recent-posts-list')
     </div>
 
-    <!-- Ads Sticky Sidebar Desktop Component -->
     <div class="col-3@md margin-top">
-
       <!-- Our Friends -->
       <div class="card padding-sm">
         <h3 class="padding-bottom-sm color-contrast-high text-md">Our Friends</h3>
         {!! Menu::get('link-exchange')->asUl() !!}
       </div>
-
-      <h3 class="padding-top-lg padding-bottom-md color-contrast-high">Related Posts</h3>
+      <!-- Popular Posts -->
+      <h3 class="padding-top-lg padding-bottom-md color-contrast-high">Popular Posts</h3>
         @include('components.posts.lists.related-posts-sidebar')
     </div>
-    
+
   </div>
 </div>
 @endsection
