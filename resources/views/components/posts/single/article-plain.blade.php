4eede63f5110477f79d7dd447aadf28085aca68c
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-03-16 19:23:27 +0400

    Tags above content in post page;

--- resources/views/components/posts/single/article-plain.blade.php
+++ resources/views/components/posts/single/article-plain.blade.php
@@ -9,28 +9,27 @@
   <img class="radius-lg" src="{{ url('/storage').config('images.posts_storage_path').$post->medium  }}" alt="Image description">
 </figure>
 
+<!-- Tags -->
+<div class="text-left text-sm">
+    @foreach(\App\Models\TagsCategories::all() as $category)
+        @if(isset($post_tags[$category->id]) && count($post_tags[$category->id]) > 0)
+            <div class="">
+                {{$category->name}}:
+                @foreach($post_tags[$category->id] as $tag)
+                    <button class="chip chip--interactive text-sm margin-bottom-xxs">
+                        <a class="link-subtle" href="/tags/{{$category->name}}/{{$tag->name}}">
+                            <i class="chip__label">{{$tag->name}}</i>
+                        </a>
+                    </button>
+                @endforeach()
+            </div>
+        @endif
+    @endforeach
+</div>
+
 <!-- Post Body -->
 <div class="text-center">
   <div class="text-component line-height-lg text-space-y-md">
     <div class="margin-top-md">{!! $post->body() !!}</div>
   </div>
-
-  <!-- Tags -->
-  <div class="text-left text-sm">
-      @foreach(\App\Models\TagsCategories::all() as $category)
-          @if(isset($post_tags[$category->id]) && count($post_tags[$category->id]) > 0)
-              <div class="">
-                  {{$category->name}}:
-                  @foreach($post_tags[$category->id] as $tag)
-                      <button class="chip chip--interactive text-sm margin-bottom-xxs">
-                          <a class="link-subtle" href="/tags/{{$category->name}}/{{$tag->name}}">
-                              <i class="chip__label">{{$tag->name}}</i>
-                          </a>
-                      </button>
-                  @endforeach()
-              </div>
-          @endif
-      @endforeach
-  </div>
-
 </div>
