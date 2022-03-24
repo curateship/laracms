92e6825d6de6c631996d12660629a0f661a37074
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-03-24 21:47:36 +0400

    Tags seeder fix; Tag edit small fix;

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -61,6 +61,7 @@ class TagFactory extends Factory
             'original' => $for_factory_updates['original'],
             'medium' => $for_factory_updates['medium'],
             'thumbnail' => $for_factory_updates['thumbnail'],
+            'body' => '{"time":1648143854001,"blocks":[{"id":"vijzrMkD7N","type":"paragraph","data":{"text":"<b>Lorem Ipsum</b> is simply dummy text of the printing and typesetting industry. <b>Lorem Ipsum</b> has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."}}],"version":"2.23.2"}'
         ];
     }
 }
