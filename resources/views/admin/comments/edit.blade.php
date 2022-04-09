60348de6e48b0031df1b604105434ec4bfe13eb4
Tyler <mobileglobalweb@gmail.com>
2022-04-09 12:08:45 -0400

    Delete comments create in admin

--- resources/views/admin/comments/edit.blade.php
+++ resources/views/admin/comments/edit.blade.php
@@ -69,63 +69,6 @@
               <p>{{ $message }}</p>
               @enderror
 
-            <!-- Select Category Dropdown Autocomplete -->
-            <div class="autocomplete position-relative select-auto js-select-auto js-autocomplete margin-bottom-sm" data-autocomplete-dropdown-visible-class="autocomplete--results-visible">
-
-              <!-- select -->
-              <select required name="post_id" class="js-select-auto__select">
-                <optgroup>
-                @foreach($posts as $key => $post)
-                <option {{ $comment->post_id === $key ? 'selected' : '' }} value="{{ $key }}">{{ $post }}</option>
-                @endforeach
-                </optgroup>
-              </select>
-
-              @error('post_id')
-                <p>{{ $message }}</p>
-              @enderror
-
-                <!-- input -->
-                <div class="select-auto__input-wrapper">
-                  <input class="form-control js-autocomplete__input js-select-auto__input" type="text" name="autocomplete-input-id" id="autocomplete-input-id" placeholder="Select a Post" autocomplete="off">
-
-                  <div class="select-auto__input-icon-wrapper">
-                    <!-- arrow icon -->
-                    <svg class="icon" viewBox="0 0 16 16">
-                      <title>Open selection</title>
-                      <polyline points="1 5 8 12 15 5" fill="none" stroke="gray" opacity="30%" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
-                    </svg>
-
-                    <!-- close X icon -->
-                    <button class="reset select-auto__input-btn js-select-auto__input-btn js-tab-focus">
-                      <svg class="icon" viewBox="0 0 16 16">
-                        <title>Reset selection</title>
-                        <path d="M8,0a8,8,0,1,0,8,8A8,8,0,0,0,8,0Zm3.707,10.293a1,1,0,1,1-1.414,1.414L8,9.414,5.707,11.707a1,1,0,0,1-1.414-1.414L6.586,8,4.293,5.707A1,1,0,0,1,5.707,4.293L8,6.586l2.293-2.293a1,1,0,1,1,1.414,1.414L9.414,8Z" />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-
-                <!-- dropdown -->
-                <div class="autocomplete__results select-auto__results js-autocomplete__results">
-                  <ul id="autocomplete1" class="autocomplete__list js-autocomplete__list">
-                    <li class="select-auto__group-title padding-y-xs padding-x-sm color-contrast-medium is-hidden js-autocomplete__result" data-autocomplete-template="optgroup" role="presentation">
-                      <span class="text-truncate text-sm" data-autocomplete-label></span>
-                    </li>
-
-                    <li class="select-auto__option padding-y-xs padding-x-sm is-hidden js-autocomplete__result" data-autocomplete-template="option">
-                      <span class="is-hidden" data-autocomplete-value></span>
-                      <div class="text-truncate" data-autocomplete-label></div>
-                    </li>
-
-                    <li class="select-auto__no-results-msg padding-y-xs padding-x-sm text-truncate is-hidden js-autocomplete__result" data-autocomplete-template="no-results" role="presentation"></li>
-                  </ul>
-                </div>
-
-                <p class="sr-only" aria-live="polite" aria-atomic="true"><span class="js-autocomplete__aria-results">0</span> results found.</p>
-              </div>
-              <!-- Select Category Dropdown Autocomplete END -->
-
                 <div class="flex justify-end gap-xs">
                       <button class="btn btn--primary">Update</button>
                     </div>
