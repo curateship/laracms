2a5e1ca9e735ea26291ba2d2732b9ddfe8e716a0
Tyler <mobileglobalweb@gmail.com>
2022-04-13 15:38:07 -0400

    Add views UI to tag show

--- resources/views/themes/default/tags/show.blade.php
+++ resources/views/themes/default/tags/show.blade.php
@@ -63,6 +63,17 @@
                              <span>{{ $post->commentsCount() }}</span>
                          </button>
                      </li>
+
+                     <li class="card-v10__social-item">
+                         <button class="reset card-v10__social-btn js-tab-focus" aria-label="Comment">
+                             <svg class="icon" viewBox="0 0 12 12">
+                                 <g>
+                                 <path d="M11.87 5.71c-0.1-0.15-2.38-3.72-5.89-3.72s-5.8 3.57-5.9 3.72a0.5 0.5 0 0 0 0 0.53c0.1 0.15 2.38 3.72 5.9 3.72s5.8-3.57 5.89-3.72a0.5 0.5 0 0 0 0-0.53z m-5.89 3.25c-2.46 0-4.3-2.21-4.88-2.98a8.31 8.31 0 0 1 2.93-2.53 2.47 2.47 0 0 0-0.54 1.53 2.49 2.49 0 0 0 4.98 0 2.47 2.47 0 0 0-0.54-1.52 8.36 8.36 0 0 1 2.92 2.52c-0.57 0.78-2.41 2.99-4.87 2.98z"></path>                                 </g>
+                             </svg>
+                             <span>554</span>
+                         </button>
+                     </li>
+
                  </ul>
              </footer>
              @empty
