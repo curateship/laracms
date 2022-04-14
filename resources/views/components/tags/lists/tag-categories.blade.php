eeada0435cdd3c3eb6cf2fedc8f0933685e48dee
Tyler <mobileglobalweb@gmail.com>
2022-04-14 16:16:56 -0400

    added link to tag category page

--- resources/views/components/tags/lists/tag-categories.blade.php
+++ resources/views/components/tags/lists/tag-categories.blade.php
@@ -1,30 +1,40 @@
 <div class="margin-bottom-md">
   <ul class="flex flex-wrap gap-xxs">
+
+  <a class="link-subtle" href="">
     <li>
       <span class="chip text-sm">
         <img class="chip__img" src="https://codyhouse.co/app/assets/img/chips-img-1.jpg" alt="Picture of Olivia Saturday">
         <i class="chip__label">Olivia Saturday</i>
       </span>
     </li>
+  </a>
 
+  <a class="link-subtle" href="">
     <li>
       <span class="chip text-sm">
         <i class="chip__label">David Smith</i>
       </span>
     </li>
+  </a>
 
+  <a class="link-subtle" href="">
     <li>
       <span class="chip text-sm">
         <img class="chip__img" src="https://codyhouse.co/app/assets/img/chips-img-1.jpg" alt="Picture of Marta Rossi">
         <i class="chip__label">Marta Rossi</i>
       </span>
     </li>
+  </a>
 
+  <a class="link-subtle" href="">
     <li>
       <span class="chip text-sm">
         <img class="chip__img" src="https://codyhouse.co/app/assets/img/chips-img-1.jpg" alt="Picture of Paul Brown">
         <i class="chip__label">Paul Brown</i>
       </span>
     </li>
+  </a>
+
   </ul>
 </div>
