0be2254a112b055c5e4c6692c51be77d4a7dc9dc
Tyler <mobileglobalweb@gmail.com>
2022-04-15 14:56:09 -0400

    Re-added theme folder

---
+++ resources/views/theme/auth/two-factor-challenge.blade.php
@@ -0,0 +1,19 @@
+@extends('theme.layouts.app')
+@section('content')
+
+<div class="container max-width-xs margin-top-xxl padding-lg card">
+ @include('components.auth.two-factor-challenge')
+</div>
+
+@parent
+@if($errors->has('email') || $errors->has('password'))
+    <script>
+    $(function() {
+        $('modal-login').modal({
+            show: true
+        });
+    });
+    </script>
+@endif
+
+@endsection
---
+++ resources/views/theme/users/edit-profile.blade.php
@@ -0,0 +1,126 @@
+@extends('theme.layouts.app')
+
+@push('custom-scripts')
+    @include('admin.users.script-js')
+@endpush
+
+@section('content')
+@include('admin.partials.alerts')
+<!-- 👇 Content Body Wrapper-->
+ <div class="container max-width-adaptive-lg">
+    <div class="grid gap-md justify-between">
+      <div class="col-12@md">
+
+        <!-- Edit Profile -->
+        <div class="card" data-table-controls="table-1">
+
+        <div class="padding-md">
+        <h3 class="padding-bottom-sm">Edit Profile</h3>
+
+        <form action="/user/edit/{{$user->id}}" method='post' autocomplete="off" >
+            @csrf
+
+        <fieldset class="margin-bottom-xxs">
+
+            <div class="margin-bottom-sm">
+            <input class="form-control width-100%" type="text" value='{{ $user->name }}' name="name" placeholder="Edit Name" required>
+            </div>
+
+            <div class="margin-bottom-sm">
+            <input class="form-control width-100%" type="text" value='{{ $user->email }}' name="email" placeholder="Edit Email" required>
+            </div>
+
+            <div class="margin-bottom-sm">
+                <div class="flex justify-between margin-bottom-xxxs">
+                <label class="form-label" for="input-password">Change Password</label>
+                </div>
+
+                <input class="form-control width-100% margin-bottom-sm" autocomplete="false" type="password" name="password" id="input-password" placeholder="Old Password">
+                <input class="form-control width-100% margin-bottom-sm" autocomplete="false" type="password" name="new-password" id="input-password" placeholder="New Password">
+                <input class="form-control width-100% margin-bottom-sm" autocomplete="false" type="password" name="confirm-password" id="input-password" placeholder="Confirm Password">
+
+
+            </div>
+        </fieldset>
+
+    <div class="flex justify-start gap-lg">
+        <!-- Edit Avatar -->
+        <div class="file-upload inline-block">
+            <label for="avatar-upload-file" class="file-upload__label btn btn--subtle">
+            <span class="flex items-center">
+              <svg class="icon" viewBox="0 0 20 20" aria-hidden="true">
+                <g fill="currentColor">
+                  <path d="M18 12v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 13V2"></path>
+                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7l5-5 5 5"></path>
+                </g>
+              </svg>
+
+              <span class="margin-left-xxs file-upload__text file-upload__text--has-max-width">Edit Avatar</span>
+            </span>
+            </label>
+
+            <input type="hidden" name="avatar-original" value="{{$user->original}}"/>
+            <input type="hidden" name="avatar-thumbnail" value="{{$user->thumbnail}}"/>
+            <input type="hidden" name="avatar-medium" value="{{$user->medium}}"/>
+
+            <input type="file" class="file-upload__input" name="image" id="avatar-upload-file" accept="image/jpeg, image/jpg, image/png, image/gif">
+
+            <br><br>
+
+            <div class="author author--featured padding-bottom-sm">
+                <div class="author__img-wrapper">
+                    <img alt="thumbnail" id="avatar-upload-thumbnail" src="{{url('/storage'.config('images.users_storage_path').$user->thumbnail)}}">
+                </div>
+            </div>
+        </div>
+
+        <!-- Edit cover -->
+        <div class="file-upload inline-block">
+            <label for="cover-upload-file" class="file-upload__label btn btn--subtle">
+            <span class="flex items-center">
+              <svg class="icon" viewBox="0 0 20 20" aria-hidden="true">
+                <g fill="currentColor">
+                  <path d="M18 12v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-4" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path>
+                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 13V2"></path>
+                  <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 7l5-5 5 5"></path>
+                </g>
+              </svg>
+
+              <span class="margin-left-xxs file-upload__text file-upload__text--has-max-width">Edit Cover</span>
+            </span>
+            </label>
+
+            <input type="hidden" name="cover-original" value="{{$user->cover_original}}"/>
+            <input type="hidden" name="cover-thumbnail" value="{{$user->cover_thumbnail}}"/>
+            <input type="hidden" name="cover-medium" value="{{$user->cover_medium}}"/>
+
+            <input type="file" class="file-upload__input" name="image" id="cover-upload-file" accept="image/jpeg, image/jpg, image/png, image/gif">
+
+            <br><br>
+
+            <img alt="thumbnail" id="cover-upload-thumbnail" src="{{url('/storage'.config('images.users_storage_path').$user->cover_thumbnail)}}">
+        </div>
+    </div>
+
+  <div class="flex justify-end gap-xs">
+        <button class="btn btn--primary">Save</button>
+      </div>
+   </form>
+</div>
+<!-- END Table-->
+
+
+
+        </div><!-- END Col-12 Card Wrapper -->
+      </div><!-- Col-12 END -->
+
+      <!-- Sidebar -->
+      <div class="col-3@md">
+        @include('components.layouts.sidebars.sidebar')
+      </div>
+      <!-- Sidebar END -->
+
+    </div><!-- Grid END (col-12 and col-3) -->
+  </div><!-- Container Wrapper END -->
+@endsection
