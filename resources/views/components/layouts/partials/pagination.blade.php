1f8798984b208dcc05745a59f9bc328be52c2c42
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-04-17 18:53:03 +0400

    Pagination fix;

--- resources/views/components/layouts/partials/pagination.blade.php
+++ resources/views/components/layouts/partials/pagination.blade.php
@@ -4,7 +4,7 @@
         <ol class="pagination__list flex flex-wrap gap-xs justify-center padding-sm">
 
             <li>
-                <a href="{{ $items->url($items->currentPage()-1) }}" class="pagination__item {{ ($items->currentPage() == 1) ? ' pagination__item--disabled' : '' }}" aria-label="Go to previous page">
+                <a href="{{ $items->appends(request()->query())->url($items->currentPage()-1) }}" class="pagination__item {{ ($items->currentPage() == 1) ? ' pagination__item--disabled' : '' }}" aria-label="Go to previous page">
                     <svg class="icon icon--xs margin-right-xxxs flip-x" viewBox="0 0 16 16"><polyline points="6 2 12 8 6 14" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>
                     <span>Prev</span>
                 </a>
@@ -12,7 +12,7 @@
 
             @if($items->currentPage() > 3)
                 <li class="display@sm">
-                    <a href="{{ $items->url(1) }}" class="pagination__item {{ ($items->currentPage() == 1) ? ' pagination__item--selected' : '' }}" aria-label="Go to page 1">1</a>
+                    <a href="{{ $items->appends(request()->query())->url(1) }}" class="pagination__item {{ ($items->currentPage() == 1) ? ' pagination__item--selected' : '' }}" aria-label="Go to page 1">1</a>
                 </li>
             @endif
 
@@ -25,7 +25,7 @@
             @foreach(range(1, $items->lastPage()) as $i)
                 @if($i >= $items->currentPage() - 2 && $i <= $items->currentPage() + 2)
                     <li class="display@sm">
-                        <a href="{{ $items->url($i) }}" class="pagination__item {{ ($items->currentPage() == $i) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $i }}">{{ $i }}</a>
+                        <a href="{{ $items->appends(request()->query())->url($i) }}" class="pagination__item {{ ($items->currentPage() == $i) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $i }}">{{ $i }}</a>
                     </li>
                 @endif
             @endforeach
@@ -38,12 +38,12 @@
 
             @if($items->currentPage() < $items->lastPage() - 2)
                 <li class="display@sm">
-                    <a href="{{ $items->url($items->lastPage()) }}" class="pagination__item {{ ($items->currentPage() == $items->lastPage()) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $items->lastPage() }}">{{ $items->lastPage() }}</a>
+                    <a href="{{ $items->appends(request()->query())->url($items->lastPage()) }}" class="pagination__item {{ ($items->currentPage() == $items->lastPage()) ? ' pagination__item--selected' : '' }}" aria-label="Go to page {{ $items->lastPage() }}">{{ $items->lastPage() }}</a>
                 </li>
             @endif
 
             <li>
-                <a href="{{ $items->url($items->currentPage()+1) }}" class="pagination__item {{ ($items->currentPage() == $items->lastPage()) ? ' pagination__item--disabled' : '' }}" aria-label="Go to next page">
+                <a href="{{ $items->appends(request()->query())->url($items->currentPage()+1) }}" class="pagination__item {{ ($items->currentPage() == $items->lastPage()) ? ' pagination__item--disabled' : '' }}" aria-label="Go to next page">
                     <span>Next</span>
                     <svg class="icon icon--xs margin-left-xxxs" viewBox="0 0 16 16"><polyline points="6 2 12 8 6 14" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>
                 </a>
