151ea69982f505c57de64f68f1ab18f8d5f0941c
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-04-20 18:06:14 +0400

    Profile page fix;

--- resources/views/components/users/user-bio.blade.php
+++ resources/views/components/users/user-bio.blade.php
@@ -1,7 +1,7 @@
 <div class="profile-author-box" style="{{$user->cover_medium != '' ? "background-image: url('".url('/storage'.config('images.users_storage_path').$user->cover_medium)."')" : ''}}">
     <!-- Author Picture -->
-    <div class="author author--featured padding-bottom-sm {{$user->thumbnail != '' ? 'profile-author-avatar' : ''}}">
-        <a href="/user/{{$user->id}}" class="author__img-wrapper">
+    <div class="author--featured padding-bottom-sm profile-author-avatar">
+        <a href="/user/{{$user->id}}" class="author__img-wrapper {{$user->thumbnail == '' ? 'with-svg-box' : ''}}">
             @if($user->thumbnail != '')
                 <img src="{{ url('/storage'.config('images.users_storage_path').$user->thumbnail) }}" alt="Author picture">
             @else
@@ -21,15 +21,15 @@
         </a>
         <h2>{{$user->name}}</h2>
     </div>
-</div>
 
-<!-- Edit profile button -->
-@auth()
-    @if($user->id == \Illuminate\Support\Facades\Auth::id())
-        <div class="flex justify-center gap-xs">
-            <a href="/user/edit">
-                <button type="button" class="btn btn--primary" data-status="draft">Edit profile</button>
-            </a>
-        </div>
-    @endif
-@endauth
+    <!-- Edit profile button -->
+    @auth()
+        @if($user->id == \Illuminate\Support\Facades\Auth::id())
+            <div class="profile-edit-button-box">
+                <a href="/user/edit">
+                    <button type="button" class="btn btn--primary" data-status="draft">Edit profile</button>
+                </a>
+            </div>
+        @endif
+    @endauth
+</div>
