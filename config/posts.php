29d3190c74749c000ce4502f6a4065e0159bb25f
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-04-29 13:26:48 +0400

    Posts auto-title methods for model and artisan`s console;

---
+++ config/posts.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'auto_title_template' => [
+        [
+            "category_id" => 8, // "characters" tags cat id in my DB;
+            "limit" => 2
+        ],
+        [" by "],
+        [
+            "category_id" => 9, // "artists" tags cat id in my DB;
+            "limit" => 1
+        ]
+    ],
+];
