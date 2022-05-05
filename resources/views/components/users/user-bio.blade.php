a2c1ca8c8f966c0763899144ca479a853c52ddfa
Tyler <mobileglobalweb@gmail.com>
2022-05-05 17:42:54 -0400

    Added follow UI to bio

--- resources/views/components/users/user-bio.blade.php
+++ resources/views/components/users/user-bio.blade.php
@@ -32,4 +32,17 @@
             </div>
         @endif
     @endauth
+    <ul class="choice-tags flex flex-wrap gap-xxs js-choice-tags profile-edit-button-box margin-right-xxl padding-right-sm">
+    <li>
+      <label class="choice-tag choice-tag--checkbox text-sm js-choice-tag" for="checkbox-tag-phone-call">
+        <input class="sr-only" type="checkbox" id="checkbox-tag-phone-call" checked>
+  
+        <svg class="choice-tag__icon icon margin-right-xxs" viewBox="0 0 16 16" aria-hidden="true"><g class="choice-tag__icon-group" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"><line x1="-6" y1="8" x2="8" y2="8" /><line x1="8" y1="8" x2="22" y2="8"/><line x1="8" y1="2" x2="8" y2="14"/></g></svg>
+  
+        <span>Follow</span>
+      </label>
+    </li>
+    </ul>
+    </div>
+
 </div>
