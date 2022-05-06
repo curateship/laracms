4cac9dc8d741d74afcc1ca35d542c08aa7836c44
Tyler <mobileglobalweb@gmail.com>
2022-05-05 21:20:44 -0400

    Add search UI to users index

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -39,12 +39,42 @@
                       </a>
                     </li>
 
-                    <li class="menu-bar__item" role="menuitem" aria-controls="post-search">
-                      <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
+                    <!-- Search -->
+                    <li class="menu-bar__item" role="menuitem" aria-controls="user-search">
+                    <svg class="icon menu-bar__icon" aria-hidden="true" viewBox="0 0 20 20">
                         <path d="M11.25 17.5c4.83 0 8.75-3.93 8.75-8.75s-3.93-8.75-8.75-8.75-8.75 3.93-8.75 8.75 3.93 8.75 8.75 8.75z m0-15c3.45 0 6.25 2.8 6.25 6.25s-2.8 6.25-6.25 6.25-6.25-2.8-6.25-6.25 2.8-6.25 6.25-6.25z"></path><path d="M0.36 17.86l3-2.99a10.02 10.02 0 0 0 1.76 1.77l-2.98 3a1.25 1.25 0 0 1-1.78 0 1.25 1.25 0 0 1 0-1.78z"></path>
-                      </svg>
-                      <span class="menu-bar__label">Search Posts</span>
+                    </svg>
+                    <span class="menu-bar__label">Search Tags</span>
                     </li>
+
+                    <!-- Search Modal -->
+                    <div class="modal modal--search modal--animate-fade bg bg-opacity-90% flex flex-center padding-md backdrop-blur-10 js-modal" id="user-search">
+                    <div class="modal__content width-100% max-width-sm max-height-100% overflow-auto" role="alertdialog" aria-labelledby="modal-search-title" aria-describedby="">
+                        <form class="full-screen-search">
+                        <label for="search-input-x" id="modal-search-title" class="sr-only">Search</label>
+                        <input class="reset full-screen-search__input" type="search" name="search-input-x" id="search-input-x" placeholder="Search...">
+                        <button class="reset full-screen-search__btn">
+                            <svg class="icon" viewBox="0 0 24 24">
+                            <title>Search</title>
+                            <g stroke-linecap="square" stroke-linejoin="miter" stroke-width="2" stroke="currentColor" fill="none" stroke-miterlimit="10">
+                                <line x1="22" y1="22" x2="15.656" y2="15.656"></line>
+                                <circle cx="10" cy="10" r="8"></circle>
+                            </g>
+                            </svg>
+                        </button>
+                        </form>
+                    </div>
+
+                    <button class="reset modal__close-btn modal__close-btn--outer  js-modal__close js-tab-focus">
+                        <svg class="icon icon--sm" viewBox="0 0 24 24">
+                        <title>Close modal window</title>
+                        <g fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <line x1="3" y1="3" x2="21" y2="21" />
+                            <line x1="21" y1="3" x2="3" y2="21" />
+                        </g>
+                        </svg>
+                    </button>
+                    </div>
                   </menu>
 
                   <menu class="menu-bar is-hidden js-int-table-actions__items-selected js-menu-bar delete-selected-users">
