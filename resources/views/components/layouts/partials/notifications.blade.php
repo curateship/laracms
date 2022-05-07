205b7aa2be394845525aa22050942cf0ddf0d129
Tyler <mobileglobalweb@gmail.com>
2022-05-06 21:47:02 -0400

    Add infinite posts component UI

---
+++ resources/views/components/layouts/partials/notifications.blade.php
@@ -0,0 +1,21 @@
+<div class="margin-top-auto border-top border-contrast-lower opacity-40%"></div><!-- Divider -->
+<div class="articles-v3__author padding-x-sm padding-top-sm">
+  <a href="#0" class="articles-v3__author-img">
+    <img class="object-cover" src="/assets/img/avatar.png" alt="Author picture">
+  </a>
+  <div class="text-component text-xs line-height-xs text-space-y-xxs">
+    <a href="/" class="color-contrast-high link-plain">
+      <p rel="author">sam mitcher commented on "This is the post that is a bit long" 3 days go</p>
+    </a>
+  </div>
+</div>
+<div class="articles-v3__author padding-x-sm padding-y-xs">
+  <a href="#0" class="articles-v3__author-img">
+    <img class="object-cover" src="/assets/img/avatar.png" alt="Author picture">
+  </a>
+  <div class="text-component text-xs line-height-xs text-space-y-xxs">
+    <a href="/" class="color-contrast-high link-plain">
+      <p rel="author">Tim Sammie just joined 3 hours go</p>
+    </a>
+  </div>
+</div>
---
+++ resources/views/theme/dashboard/index.blade.php
@@ -0,0 +1,30 @@
+@extends('admin.layouts.app')
+@section('content')
+
+<div class="container max-width-adaptive-lg">
+  <div class="grid gap-md justify-between">
+
+    <!-- Col 3 -->
+    <div class="col-4@md card">
+      <p class="padding-sm">Latest Feeds</p>
+      @include('components.layouts.partials.notifications')
+    </div>
+
+    <!-- Col 8 -->
+    <div class="col-8@md card">
+      @include('components.posts.lists.infinite-posts')
+    </div>
+
+    <!-- 👇 Col 3 -->
+    <div class="col-3@md">
+        @if(\Illuminate\Support\Facades\Gate::allows('is-admin'))
+            @include('admin.partials.sidebar-admin')
+        @else
+            @include('admin.partials.sidebar')
+        @endif
+    </div>
+
+  </div><!-- Grid END -->
+</div>
+
+@endsection
