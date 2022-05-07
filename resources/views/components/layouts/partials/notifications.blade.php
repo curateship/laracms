5af3247e54c57bd550b90648b7c843cb376ad57a
Tyler <mobileglobalweb@gmail.com>
2022-05-07 13:36:47 -0400

    Added Notifications UI

--- resources/views/components/layouts/partials/notifications.blade.php
+++ resources/views/components/layouts/partials/notifications.blade.php
@@ -1,21 +1,78 @@
-<div class="margin-top-auto border-top border-contrast-lower opacity-40%"></div><!-- Divider -->
-<div class="articles-v3__author padding-x-sm padding-top-sm">
-  <a href="#0" class="articles-v3__author-img">
-    <img class="object-cover" src="/assets/img/avatar.png" alt="Author picture">
-  </a>
-  <div class="text-component text-xs line-height-xs text-space-y-xxs">
-    <a href="/" class="color-contrast-high link-plain">
-      <p rel="author">sam mitcher commented on "This is the post that is a bit long" 3 days go</p>
+<ul class="notif ">
+
+  <li class="notif__item ">
+    <a class="notif__link flex padding-sm" href="#0">
+      <figure class="notif__figure margin-right-xs color-primary" aria-hidden="true">
+        <img src="https://codyhouse.co/app/assets/img/notifications-img-2.jpg" alt="user picture">
+      </figure>
+
+      <div class="flex-grow margin-right-xs">
+
+        <div class="padding-top-xxxxs">
+          <p class="text-sm"><i class="font-semibold">Olivia Saturday</i> commented on your <i class="font-semibold">"This is all it takes to improve..."</i> post.</p>
+          <p class="text-xs color-contrast-medium margin-top-xxxxs"><time>1 hour ago</time></p>
+        </div>
+      </div>
+
+      <div class="notif__dot margin-left-auto" aria-hidden="true"></div>
     </a>
-  </div>
-</div>
-<div class="articles-v3__author padding-x-sm padding-y-xs">
-  <a href="#0" class="articles-v3__author-img">
-    <img class="object-cover" src="/assets/img/avatar.png" alt="Author picture">
-  </a>
-  <div class="text-component text-xs line-height-xs text-space-y-xxs">
-    <a href="/" class="color-contrast-high link-plain">
-      <p rel="author">Tim Sammie just joined 3 hours go</p>
+  </li>
+
+  <li class="notif__item ">
+    <a class="notif__link flex padding-sm" href="#0">
+      <figure class="notif__figure margin-right-xs color-accent" aria-hidden="true">
+        <img src="https://codyhouse.co/app/assets/img/notifications-img-3.jpg" alt="user picture">
+      </figure>
+
+      <div class="flex-grow margin-right-xs">
+
+        <div class="padding-top-xxxxs">
+          <p class="text-sm">It's <i class="font-semibold">David Smith</i>'s birthday. Wish him well!</p>
+          <p class="text-xs color-contrast-medium margin-top-xxxxs"><time>12 hours ago</time></p>
+        </div>
+      </div>
+
+      <div class="notif__dot margin-left-auto" aria-hidden="true"></div>
     </a>
-  </div>
-</div>
+  </li>
+
+  <li class="notif__item ">
+    <a class="notif__link flex padding-sm" href="#0">
+      <figure class="notif__figure margin-right-xs color-primary" aria-hidden="true">
+        <img src="https://codyhouse.co/app/assets/img/notifications-img-1.jpg" alt="user picture">
+      </figure>
+
+      <div class="flex-grow margin-right-xs">
+
+        <div class="padding-top-xxxxs">
+          <p class="text-sm"><i class="font-semibold">Marta Rossi</i> posted <i class="font-semibold">"10 helpful tips to learn web design"</i>.</p>
+          <p class="text-xs color-contrast-medium margin-top-xxxxs"><time>a day ago</time></p>
+
+          <div class="bg-light radius-md padding-xs inner-glow shadow-sm margin-top-sm text-sm">
+            <p class="color-contrast-medium line-height-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum beatae commodi quibusdam officiis...</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="notif__dot margin-left-auto" aria-hidden="true"></div>
+    </a>
+  </li>
+
+  <li class="notif__item ">
+    <a class="notif__link flex padding-sm" href="#0">
+      <figure class="notif__figure margin-right-xs color-primary" aria-hidden="true">
+        <img src="https://codyhouse.co/app/assets/img/notifications-img-4.jpg" alt="user picture">
+      </figure>
+
+      <div class="flex-grow margin-right-xs">
+
+        <div class="padding-top-xxxxs">
+          <p class="text-sm"><i class="font-semibold">Paul Brown</i> liked your photo.</p>
+          <p class="text-xs color-contrast-medium margin-top-xxxxs"><time>1 week ago</time></p>
+        </div>
+      </div>
+
+    </a>
+  </li>
+
+</ul>
--- resources/views/theme/dashboard/index.blade.php
+++ resources/views/theme/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container max-width-adaptive-lg">
   <div class="grid gap-md justify-between">
 
-    <!-- Col 3 -->
+    <!-- Col 4 -->
     <div class="col-4@md card">
       <p class="padding-sm">Latest Feeds</p>
       @include('components.layouts.partials.notifications')
@@ -15,7 +15,7 @@
       @include('components.posts.lists.infinite-posts')
     </div>
 
-    <!-- 👇 Col 3 -->
+    <!-- Col 3 -->
     <div class="col-3@md">
         @if(\Illuminate\Support\Facades\Gate::allows('is-admin'))
             @include('admin.partials.sidebar-admin')
@@ -24,7 +24,7 @@
         @endif
     </div>
 
-  </div><!-- Grid END -->
+  </div>
 </div>
 
 @endsection
