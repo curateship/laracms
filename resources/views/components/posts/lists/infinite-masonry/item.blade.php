0e80d88f9d99f2ef2bec6a65778caf42f3e1e75f
Tyler <mobileglobalweb@gmail.com>
2022-05-17 19:17:56 -0400

    Updated masonry UI

--- resources/views/components/posts/lists/infinite-masonry/item.blade.php
+++ resources/views/components/posts/lists/infinite-masonry/item.blade.php
@@ -1,22 +1,16 @@
 @foreach($posts as $post)
 <div class="grid-item">
     @if($post->thumbnail != '')
-    <a class="thumb" href="{{ route('post.show', $post) }}">
-        <figure class="card-v2">
-            <img class="block width-500% radius-md radius-bottom-right-0 radius-bottom-left-0" src="{{ url('/storage').config('images.posts_storage_path').$post->thumbnail }}" alt="Image of {{ $post->title }}">
-            <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-left">
-
-            </figcaption>
-        </figure>
-    </a>
+    <figure class="card__img img-blend" data-blend-pattern="0,0,1,0" data-blend-color="--color-bg-light" data-blend-height="45%">
+      <a class="" href="{{ route('post.show', $post) }}">
+        <img class="radius-md radius-bottom-right-0 radius-bottom-left-0" src="{{ url('/storage').config('images.posts_storage_path').$post->thumbnail }}" alt="Image of {{ $post->title }}">
+      </a>
+    </figure>
     @else
-    <span class="card__img card__img-cropped bg-opacity-50%"></span>
-    <div class="post-cell text-component line-height-xs v-space-xxs">
-
     </div>
+
     @endif
-    <div class="user-cell bg-black radius-md radius-top-right-0 radius-top-left-0">
+    <div class="bg-dark radius-md radius-top-right-0 radius-top-left-0">
         <h3 class="padding-sm"><a class="color-contrast-medium link-plain" href="{{ route('post.show', $post) }}">{{ $post->title }}</a></h3>
     </div>
-</div>
 @endforeach
