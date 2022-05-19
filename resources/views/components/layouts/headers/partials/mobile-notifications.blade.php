eaf800cab19223c0cabe1b085954afaa9ba324c8
Tyler <mobileglobalweb@gmail.com>
2022-05-19 14:01:44 -0400

    Change clear trash can notification color

--- resources/views/components/layouts/headers/partials/mobile-notifications.blade.php
+++ resources/views/components/layouts/headers/partials/mobile-notifications.blade.php
@@ -18,7 +18,7 @@
 
       <!-- Delete -->
       <li class="menu-bar__item" role="menuitem">
-          <svg class="icon" aria-hidden="true" viewBox="0 0 16 16">
+          <svg class="icon color-contrast-low" aria-hidden="true" viewBox="0 0 16 16">
             <g><path d="M2,6v8c0,1.1,0.9,2,2,2h8c1.1,0,2-0.9,2-2V6H2z"></path><path d="M12,3V1c0-0.6-0.4-1-1-1H5C4.4,0,4,0.4,4,1v2H0v2h16V3H12z M10,3H6V2h4V3z"></path></g>
           </svg>
           <span class="menu-bar__label">Clear</span>
