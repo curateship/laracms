9a312ea431b3ba94903b50aa24cbf877b5f35fb0
Tyler <mobileglobalweb@gmail.com>
2022-05-24 11:40:07 -0400

    Auto title and 12 post index change

--- config/posts.php
+++ config/posts.php
@@ -5,6 +5,11 @@ return [
             "category_id" => 8, // "characters" tags cat id in my DB;
             "limit" => 2
         ],
+        ["   "],
+        [
+          "category_id" => 10, // "MISC" tags cat id in my DB;
+          "limit" => 4
+        ],
         [" by "],
         [
             "category_id" => 9, // "artists" tags cat id in my DB;
