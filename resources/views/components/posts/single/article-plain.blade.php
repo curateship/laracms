15cdff6cf1297c0bb072686eec1758483743ec89
Tyler <mobileglobalweb@gmail.com>
2022-05-24 19:36:26 -0400

    Fix article-plain UI

--- resources/views/components/posts/single/article-plain.blade.php
+++ resources/views/components/posts/single/article-plain.blade.php
@@ -10,24 +10,26 @@
 
 <!-- Post Body -->
 <div class="text-center">
-  <div class="text-component line-height-lg text-space-y-xl">
-    {!! $post->body() !!}
+  <div class="text-component line-height-lg text-space-y-md">
+    <div class="margin-top-md">{!! $post->body() !!}</div>
   </div>
 
   <!-- Tags -->
   <div class="text-left text-sm padding-top-md">
-      @foreach(\App\Models\TagsCategories::all() as $category)
-          @if(isset($post_tags[$category->id]) && count($post_tags[$category->id]) > 0)
+    @foreach(\App\Models\TagsCategories::all() as $category)
+        @if(isset($post_tags[$category->id]) && count($post_tags[$category->id]) > 0)
+          <div class="">
             {{$category->name}}:
               @foreach($post_tags[$category->id] as $tag)
                 <button class="chip chip--interactive text-sm margin-bottom-xxs">
-                     <a class="link-subtle" href="/tags/{{$category->name}}/{{$tag->slug}}">
-                         <i class="chip__label">{{$tag->name}}</i>
-                      </a>
+                  <a class="link-subtle" href="/tags/{{$category->name}}/{{$tag->slug}}">
+                    <i class="chip__label">{{$tag->name}}</i>
+                            </a>
                 </button>
-                @endforeach()
-          @endif
-      @endforeach
+              @endforeach()
+          </div>
+        @endif
+    @endforeach
   </div>
 </div>
 
