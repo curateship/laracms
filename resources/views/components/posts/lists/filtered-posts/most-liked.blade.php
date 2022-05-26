7005c4cf2f199da3691a19127122466dd00aee73
Tyler <mobileglobalweb@gmail.com>
2022-05-26 15:40:34 -0400

    Added most liked posts UI

---
+++ resources/views/components/posts/lists/filtered-posts/most-liked.blade.php
@@ -0,0 +1,25 @@
+<ul class="grid-auto-xs grid-auto-sm@sm grid-auto-md@md gap-md">
+<li class="card">
+ <div class="">
+  <a href="">
+    <figure class="aspect-ratio-4:3 margin-bottom-xs">
+     <img class="block width-100%" loading="lazy" src="" alt="Image description">
+    </figure>
+  </a>
+    <div class="card__content recent-post-card line-height-1 margin-xxs">
+     <a href="" class="link-subtle text-sm">Post title</a>
+        <div class="flex justify-between" style="align-items: baseline">
+            <div class="text-xs color-contrast-low padding-top-sm">4 days ago</div>
+            <button class="reset card-v10__social-btn" aria-label="Comment" style="outline: none;width: auto">
+              <svg class="icon" viewBox="0 0 12 12">
+                <g>
+                  <path d="M11.045,2.011a3.345,3.345,0,0,0-4.792,0c-.075.075-.15.225-.225.3-.075-.074-.15-.224-.225-.3a3.345,3.345,0,0,0-4.792,0,3.345,3.345,0,0,0,0,4.792l5.017,4.718L11.045,6.8A3.484,3.484,0,0,0,11.045,2.011Z"></path>
+                </g>
+              </svg>
+                <span>32</span>
+            </button>
+        </div>
+    </div>
+ </div>
+</ul>
+
