856fc6661cd43e0ede5ba43914b2f9c5eb2ee006
Tyler <mobileglobalweb@gmail.com>
2022-05-31 19:34:39 -0400

    Adjusted post config parameter

--- config/posts.php
+++ config/posts.php
@@ -3,14 +3,13 @@ return [
     'auto_title_template' => [
         [
             "category_id" => 8, // "characters" tags cat id in my DB;
-            "limit" => 2
+            "limit" => 3
         ],
-        ["   "],
         [
-          "category_id" => 10, // "MISC" tags cat id in my DB;
-          "limit" => 4
+            "category_id" => 10, // "MISC" tags cat id in my DB;
+            "limit" => 4
         ],
-        [" by "],
+        ["by"],
         [
             "category_id" => 9, // "artists" tags cat id in my DB;
             "limit" => 1
