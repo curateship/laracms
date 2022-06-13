ae4bb10492dce1e5d8c91550ff083a96eb0679ca
Tyler <mobileglobalweb@gmail.com>
2022-06-13 13:06:43 -0400

    Filtered posts sort by time UI

--- resources/views/theme/posts/most-viewed.blade.php
+++ resources/views/theme/posts/most-viewed.blade.php
@@ -4,8 +4,24 @@
 <!-- Container -->
 <div class="container max-width-adaptive-lg padding-top-sm">
 
-<div class="text-component padding-bottom-md">
+<div class="flex flex-column text-component padding-bottom-md gap-xs flex-row@md justify-between@md items-center@md">
     <h1 class="text-xl">Most viewed posts</h1>
+
+<!-- Date Filter -->
+<div class="inline-flex items-baseline">
+  <label class="text-sm color-contrast-medium margin-right-xs" for="selectThis">Sort:</label>
+  
+  <div class="select inline-block js-select" data-trigger-class="reset text-sm color-contrast-high text-underline inline-flex items-center cursor-pointer js-tab-focus">
+    <select name="selectThis" id="selectThis">
+      <option value="0" selected>All Time</option>
+      <option value="1">Today</option>
+      <option value="2">Week</option>
+      <option value="4">Month</option>
+    </select>
+    
+    <svg class="icon icon--xxxs margin-left-xxs" viewBox="0 0 8 8"><path d="M7.934,1.251A.5.5,0,0,0,7.5,1H.5a.5.5,0,0,0-.432.752l3.5,6a.5.5,0,0,0,.864,0l3.5-6A.5.5,0,0,0,7.934,1.251Z"/></svg>
+  </div>
+</div>
 </div>
 
 <!-- Search Results -->
