4ccc1b04301de1b424c5cd00e47fc90ac2e7d554
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-06-15 23:24:30 +0400

    Most posts filters;

--- resources/views/theme/posts/most-liked.blade.php
+++ resources/views/theme/posts/most-liked.blade.php
@@ -1,4 +1,9 @@
 @extends('theme.layouts.app')
+
+@push('custom-scripts')
+    @include('theme.posts.most-posts-scripts')
+@endpush
+
 @section('content')
 
 <!-- Container -->
@@ -10,15 +15,15 @@
 <!-- Date Filter -->
 <div class="inline-flex items-baseline">
   <label class="text-sm color-contrast-medium margin-right-xs" for="selectThis">Sort:</label>
-  
+
   <div class="select inline-block js-select" data-trigger-class="reset text-sm color-contrast-high text-underline inline-flex items-center cursor-pointer js-tab-focus">
-    <select name="selectThis" id="selectThis">
-      <option value="0" selected>All Time</option>
-      <option value="1">Today</option>
-      <option value="2">Week</option>
-      <option value="4">Month</option>
+      <select name="selectThis" id="selectThis" class="most-posts-filter">
+          <option value="/most-liked" {{request()->get('filter') == null ? 'selected' : ''}}>All Time</option>
+          <option value="/most-liked?filter=today" {{request()->get('filter') == 'today' ? 'selected' : ''}}>Today</option>
+          <option value="/most-liked?filter=week" {{request()->get('filter') == 'week' ? 'selected' : ''}}>Week</option>
+          <option value="/most-liked?filter=month" {{request()->get('filter') == 'month' ? 'selected' : ''}}>Month</option>
     </select>
-    
+
     <svg class="icon icon--xxxs margin-left-xxs" viewBox="0 0 8 8"><path d="M7.934,1.251A.5.5,0,0,0,7.5,1H.5a.5.5,0,0,0-.432.752l3.5,6a.5.5,0,0,0,.864,0l3.5-6A.5.5,0,0,0,7.934,1.251Z"/></svg>
   </div>
 </div>
