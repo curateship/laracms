4e0e1577ffce376b17764892caf2358996e37eda
Tyler <typham2@gmail.com>
2022-07-26 15:25:07 -0400

    Rearranged site discription and addded padding

--- resources/views/components/layouts/partials/homepage-site-description.blade.php
+++ resources/views/components/layouts/partials/homepage-site-description.blade.php
@@ -1,5 +1,3 @@
-<div class="container max-width-adaptive-lg padding-top-sm">
-
 <article class="article text-component">
   <h2 class="padding-bottom-sm color-contrast-medium">The Best Hentai Exprience. We prioritize quality!</h2>
   <p class="color-contrast-low">HentaiVice provides you with videos from all your favorites Anime and Video games parody characters such as League of Legends to Naruto. We provide the highest quality possible. We update daily with the freshest content so please do come back to experience the best HentaiVice has to offer. We have everything you need all categorize neatly with tags. We have fetishes from Futanari (dick girls) to Furries and everything in between.</p>
@@ -10,5 +8,3 @@
   <h2 class="padding-bottom-sm color-contrast-medium">We want your feedback!</h2>
   <p class="color-contrast-low">We at HentaiVice are always looking to improve our site and our users experience, if you have any suggestions or concern. You can email us and we'll try and get back to you as soon as possible.</p>
 </article>
-
-</div>
--- resources/views/theme/index/full-width.blade.php
+++ resources/views/theme/index/full-width.blade.php
@@ -3,23 +3,24 @@
 
 <div class="container max-width-adaptive-lg">
 
-    <!-- Popular Tags -->
-    <div class="padding-y-sm">
-      @include('components.tags.lists.popular-tags')
-    </div>
-
     <!-- Recent Posts -->
     <div class="margin-bottom-lg">
       @include('components.posts.lists.recent-posts.recent-posts-grid')
     </div>
 
     <!-- Pagination -->
-    <div class="padding-bottom-md">
+    <div class="padding-bottom-xl">
         @include('components.layouts.partials.pagination', ['items' => $recent_posts])
     </div>
 
+    <!-- Popular Tags -->
+    <h1 class="padding-bottom-sm">Popular Tags</h1>
+    <div class="padding-y-md">
+      @include('components.tags.lists.popular-tags')
+    </div>
+
     <!-- Site Discription -->
-    <div class="padding-bottom-md">
+    <div class="padding-y-md">
         @include('components.layouts.partials.homepage-site-description')
     </div>
 
