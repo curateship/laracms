05cb4250997a6d81417e22a2133cfe631a77bc51
Tyler <typham2@gmail.com>
2022-07-31 19:35:10 -0400

    Fix SEO title

--- config/seotools.php
+++ config/seotools.php
@@ -20,7 +20,7 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "HentaiRing", // set false to total remove
+            'title'        => "Hentai Ring", // set false to total remove
 
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
             'description'  => false, // set false to total remove
