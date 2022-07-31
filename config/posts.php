9dfa5d7e065abf8a1bb121bb00b89e71dbea494c
Tyler <typham2@gmail.com>
2022-07-31 17:29:17 -0400

    Update auto title

--- config/posts.php
+++ config/posts.php
@@ -5,6 +5,11 @@ return [
             "category_id" => 8, // "characters" tags cat id in my DB;
             "limit" => 3
         ],
+        ["from"],
+        [
+            "category_id" => 6, // "origin" tags cat id in my DB;
+            "limit" => 2
+        ]
         [
             "category_id" => 10, // "MISC" tags cat id in my DB;
             "limit" => 4
