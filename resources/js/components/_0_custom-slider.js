b2575505666b1e144a76f51563fe1ed3f07a6e9b
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-07-31 23:59:27 +0400

    New tags slider;

--- resources/js/components/_0_custom-slider.js
+++ resources/js/components/_0_custom-slider.js
@@ -1,56 +1,59 @@
 const ele = document.getElementById('slider-box');
-let pos = { top: 0, left: 0, x: 0, y: 0 };
-let scrollDirection
 
-const mouseDownHandler = function (e) {
-    ele.style.cursor = 'grabbing';
-    ele.style.userSelect = 'none';
+if(ele !== null){
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+    let scrollDirection
 
-    pos = {
-        left: ele.scrollLeft,
-        x: e.clientX,
+    const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+            left: ele.scrollLeft,
+            x: e.clientX,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
     };
 
-    document.addEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mouseup', mouseUpHandler);
-};
-
-const mouseMoveHandler = function (e) {
-    const dx = e.clientX - pos.x;
-    const newScrollPost = pos.left - dx;
-
-    if(ele.scrollLeft > newScrollPost){
-        scrollDirection = 'left'
-    }   else{
-        scrollDirection = 'right'
-    }
-
-    ele.scrollLeft = newScrollPost
-};
-
-const mouseUpHandler = function (e) {
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.removeEventListener('mouseup', mouseUpHandler);
-
-    ele.style.cursor = 'grab';
-    ele.style.removeProperty('user-select');
-
-    setTimeout(function(){
+    const mouseMoveHandler = function (e) {
         const dx = e.clientX - pos.x;
-        if(scrollDirection === 'right'){
-            ele.scrollLeft = pos.left - dx + 40;
+        const newScrollPost = pos.left - dx;
+
+        if(ele.scrollLeft > newScrollPost){
+            scrollDirection = 'left'
         }   else{
-            ele.scrollLeft = pos.left - dx - 40;
+            scrollDirection = 'right'
         }
-    }, 1)
-};
 
-ele.addEventListener('mousedown', mouseDownHandler);
+        ele.scrollLeft = newScrollPost
+    };
 
-function scrollWithArrow(direction){
-    if(direction === 'right'){
-        ele.scrollLeft += 150;
-    }   else{
-        ele.scrollLeft -= 150;
+    const mouseUpHandler = function (e) {
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        setTimeout(function(){
+            const dx = e.clientX - pos.x;
+            if(scrollDirection === 'right'){
+                ele.scrollLeft = pos.left - dx + 40;
+            }   else{
+                ele.scrollLeft = pos.left - dx - 40;
+            }
+        }, 1)
+    };
+
+    ele.addEventListener('mousedown', mouseDownHandler);
+
+    function scrollWithArrow(direction){
+        if(direction === 'right'){
+            ele.scrollLeft += 150;
+        }   else{
+            ele.scrollLeft -= 150;
+        }
     }
 }
