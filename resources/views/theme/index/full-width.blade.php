f707f08323987d4b91afb5446c8d84bc1bf4ebb4
Tyler <typham2@gmail.com>
2022-07-31 12:06:37 -0400

    Updated colors and popular tag UI

--- resources/views/theme/index/full-width.blade.php
+++ resources/views/theme/index/full-width.blade.php
@@ -12,5 +12,17 @@
     <div class="padding-bottom-xl">
         @include('components.layouts.partials.pagination', ['items' => $recent_posts])
     </div>
+
+    <!-- Popular Tags -->
+    <h1 class="padding-bottom-sm">Popular Tags</h1>
+    <div class="padding-y-md">
+      @include('components.tags.lists.popular-tags')
+    </div>
+
+    <!-- Site Discription -->
+    <div class="padding-y-md">
+        @include('components.layouts.partials.homepage-site-description')
+    </div>
+
 </div>
 @endsection
