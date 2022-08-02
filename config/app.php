99509bab5584ccc3e8abed7c6161cfff2df417a6
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-08-02 18:06:44 +0400

    Theme in env;

--- config/app.php
+++ config/app.php
@@ -1,8 +1,7 @@
 <?php
 
 return [
-
-    'default_theme' => 'dark',
+    'default_theme' => env('DEFAULT_COLOR', 'dark'),
     'name' => env('APP_NAME', 'Laravel'),
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
