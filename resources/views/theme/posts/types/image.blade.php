1a7348ff2aa9788ebf82fcf2ae762db809fe6344
Tyler <typham2@gmail.com>
2022-08-10 18:04:04 -0400

    Update pending review UI

--- resources/views/theme/posts/types/image.blade.php
+++ resources/views/theme/posts/types/image.blade.php
@@ -15,9 +15,30 @@
   <!-- Content -->
   <div class="col-11@md padding-x-lg@md">
       @if($post->user_id == \Illuminate\Support\Facades\Auth::id() && $post->status == 'pending')
-          <div class="margin-sm padding-sm text-center post-pending-message">
-              Your post in pending review
+
+      <aside class="note note--warning margin-bottom-md">
+          <div class="flex items-center">
+            <svg class="icon icon--md margin-right-sm" viewBox="0 0 30 30">
+              <path d="M12.4 2.563L.377 24.518A3.023 3.023 0 0 0 3.006 29h23.987a3.023 3.023 0 0 0 2.632-4.477L17.66 2.569a2.992 2.992 0 0 0-5.26-.006z" fill="var(--color-warning-dark)" opacity=".25"></path>
+              <path fill="none" stroke="var(--color-warning-dark)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19v-9"></path>
+              <circle cx="15" cy="23.5" r="1.5" fill="var(--color-warning-dark)"></circle>
+            </svg>
+
+            <p class="note__title  color-contrast-higher"><strong>Your post in pending review</strong></p>
           </div>
+
+          <div class="flex margin-top-xxxs">
+            <!-- spacer - occupy same space of icon above 👆 -->
+            <svg class="icon icon--md margin-right-sm" aria-hidden="true"></svg>
+
+            <div class="note__content text-component">
+              <!-- note content -->
+              <p>To ensure quality on our site. All posts must be approved</p>
+              <!-- end note content -->
+            </div>
+          </div>
+        </aside>
+        
       @endif
 
     @include('components.posts.show.types.image')
