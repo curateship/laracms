33635c8af3210d6c40169a04fd40367732e4da6f
Tyler <typham2@gmail.com>
2022-09-26 10:33:59 -0400

    contest UI

--- resources/views/admin/contests/show.blade.php
+++ resources/views/admin/contests/show.blade.php
@@ -1,9 +1,19 @@
 @extends(config('theme.admin_app_template'))
 
 @section('content')
-    <div class="grid gap-md justify-between">
-        <div class="col-12@md">
-            {!! $contest->getContent() !!}
-        </div>
+<!-- Post Image With Gradiant -->
+<figure class="card__img img-blend opacity-20%" data-blend-pattern="0,0,1,0" data-blend-color="--color-bg-light" data-blend-height="100%">
+    <img class="radius-md post-image" src="https://codyhouse.co/app/assets/img/how-it-works-v4-img-1.jpg" alt="Card preview img">
+</figure>
+
+<!-- Post Body -->
+<div class="post-content padding-md">
+  <div class="flex">
+    <div class="post-content">
+        <div class="margin-top-md">{!! $contest->getContent() !!}</div>
     </div>
+  </div>
+</div>
 @endsection
+
+
