f17bc55f0e0ad4a8e8e3e15ab2b0deff842b79a0
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2022-10-22 15:53:02 +0400

    Contests status group;

--- resources/views/components/contests/list.blade.php
+++ resources/views/components/contests/list.blade.php
@@ -2,21 +2,26 @@
 @section('content')
     <div class="padding-md">
         <div class="margin-bottom-lg">
-            <ul class="grid-auto-xs grid-auto-sm@sm grid-auto-lg@md gap-md">
-                @foreach($contests as $contest)
-                    <li class="card">
-                        <a href="/contest/{{$contest->slug}}">
-                            <figure class="aspect-ratio-4:3 margin-bottom-xs">
-                                <img class="block width-100% radius-md radius-bottom-right-0 radius-bottom-left-0" loading="lazy" src="{{ url('/storage').config('images.contests_storage_path').$contest->thumbnail  }}" alt="Image description">
-                            </figure>
-                        </a>
-                        <div class="card__content recent-post-card line-height-1 margin-xxs">
-                            <a href="/contest/{{$contest->slug}}" class="link-subtle text-sm">{{$contest->title}}</a>
-                            <p class="text-xs color-contrast-low padding-top-sm">{{ $contest->created_at->diffforhumans() }}<br></p>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
+            @foreach($contests_list as $status => $contests)
+                @if(count($contests) > 0)
+                    <h3 class="margin-bottom-sm contest-status contest-status-{{$status}}">{{$status}}</h3>
+                    <ul class="grid-auto-xs grid-auto-sm@sm grid-auto-lg@md gap-md margin-bottom-md">
+                    @foreach($contests as $contest)
+                        <li class="card">
+                            <a href="/contest/{{$contest->slug}}">
+                                <figure class="aspect-ratio-4:3 margin-bottom-xs">
+                                    <img class="block width-100% radius-md radius-bottom-right-0 radius-bottom-left-0" loading="lazy" src="{{ url('/storage').config('images.contests_storage_path').$contest->thumbnail  }}" alt="Image description">
+                                </figure>
+                            </a>
+                            <div class="card__content recent-post-card line-height-1 margin-xxs">
+                                <a href="/contest/{{$contest->slug}}" class="link-subtle text-sm">{{$contest->title}}</a>
+                                <p class="text-xs color-contrast-low padding-top-sm">{{ $contest->created_at->diffforhumans() }}<br></p>
+                            </div>
+                        </li>
+                    @endforeach
+                    </ul>
+                @endif
+            @endforeach
         </div>
     </div>
 @endsection
